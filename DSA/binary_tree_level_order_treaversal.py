dac47d4c60a7b1948a6bb78b6e5df48a8bbd87a2
Ayushi Tyagi <ayushityagi794@gmail.com>
2026-07-17 23:59:42 +0530

    LEETCODE_102

---
+++ DSA/binary_tree_level_order_treaversal.py
@@ -0,0 +1,25 @@
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        res = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            curr_level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                curr_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(curr_level)
+
+        return res
+        
