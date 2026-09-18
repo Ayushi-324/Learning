c2d4c22b1822e669abdc019da196f95735c0535a
Ayushi Tyagi <ayushityagi794@gmail.com>
2026-09-18 23:59:41 +0530

    LEETCODE_624
    Implement maxDistance method to calculate maximum distance between elements of different arrays.

---
+++ DSA/max_distance_in_arr.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxDistance(self, arrays: list[list[int]]) -> int:
+        min_val = arrays[0][0] #phle arr se min & max start kr
+        max_val = arrays[0][-1]
+        max_dist = 0
+
+        for i in range(1, len(arrays)):  #loop on other arrays
+            current_min = arrays[i][0]
+            current_max = arrays[i][-1]
+            
+            max_dist = max(max_dist,   #cross-diff check krke max dist update
+                           abs(current_max - min_val), 
+                           abs(max_val - current_min))
+    
+            min_val = min(min_val, current_min) #global min nd max upda for next iteration
+            max_val = max(max_val, current_max)
+            
+        return max_dist
+        
