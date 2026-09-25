1e9f473feb8484fdc6064751fadf764076461095
Ayushi Tyagi <ayushityagi794@gmail.com>
2026-09-25 23:09:53 +0530

    LEETCODE_2913
    Implement a method to calculate the sum of squares of distinct elements in all subarrays.

---
+++ DSA/subarr_distinct_elem_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumCounts(self, nums: List[int]) -> int:
+        total_sum = 0
+        n = len(nums)
+
+        for i in range(n):  #outer loop subarr start 
+            distinct_elem = set()  #har br naya khali set 
+
+            for j in range(i, n):  #inner loop subarr end 
+                distinct_elem.add(nums[j]) # set removes dumplicate 
+
+                count = len(distinct_elem)  #take unique elem count - do square add in sum  
+                total_sum += count * count
+
+        return total_sum
